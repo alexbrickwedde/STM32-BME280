e8a146b34364e6f7fe40521c2c1907aa12171042
Alex Brickwedde <alex@brickwedde.de>
2022-01-10 19:51:27 +0100

    x

--- Inc/bme280.h
+++ Inc/bme280.h
@@ -1,6 +1,6 @@
 #include <stm32hal.h>
+
 #include "../BME280_driver/bme280.h"
-#include "../BME280_driver/bme280_defs.h"
 
 int8_t BME280_Init(uint8_t address, I2C_HandleTypeDef* hi2c, TIM_TypeDef *htim);
-uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef* hi2c);
+uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef* hi2c, struct bme280_data *comp_data);
--- Src/bme280.c
+++ Src/bme280.c
@@ -4,7 +4,6 @@
 struct identifier
 {
     uint8_t dev_addr;
-    int8_t fd;
     I2C_HandleTypeDef *hi2c;
     TIM_HandleTypeDef *htim;
 };
@@ -16,25 +15,29 @@ struct identifier id;
 int8_t BME280_user_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr)
 {
   struct identifier *pid = intf_ptr;
-  return 0;
+  HAL_StatusTypeDef Result = HAL_I2C_Mem_Read(pid->hi2c, pid->dev_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, len, 100);
+  return Result == HAL_OK ? BME280_INTF_RET_SUCCESS : BME280_E_COMM_FAIL;
 }
 
 int8_t BME280_user_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr)
 {
   struct identifier *pid = intf_ptr;
-  return 0;
+  HAL_StatusTypeDef Result = HAL_I2C_Mem_Write(pid->hi2c, pid->dev_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, len, 100);
+  return Result == HAL_OK ? BME280_INTF_RET_SUCCESS : BME280_E_COMM_FAIL;
 }
 
 void BME280_user_delay_us(uint32_t period, void *intf_ptr)
 {
   struct identifier *pid = intf_ptr;
-  delay_usec(pid->htim, period);
+//  delay_usec(pid->htim, period);
+  HAL_Delay((period / 1000) + 1);
 }
 
 int8_t BME280_Init(uint8_t address, I2C_HandleTypeDef *hi2c, TIM_TypeDef *htim) {
 
   id.hi2c = hi2c;
   id.dev_addr = BME280_I2C_ADDR_PRIM;
+  id.htim = htim;
 
   dev.intf = BME280_I2C_INTF;
   dev.read = BME280_user_i2c_read;
@@ -49,7 +52,7 @@ int8_t BME280_Init(uint8_t address, I2C_HandleTypeDef *hi2c, TIM_TypeDef *htim) 
 // HAL_I2C_Master_Transmit(hi2c, address << 1, buffer, 1, 1000);
 }
 
-uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef *hi2c) {
+uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef *hi2c, struct bme280_data *comp_data) {
   /* Variable to define the result */
   int8_t rslt = BME280_OK;
 
@@ -60,7 +63,6 @@ uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef *hi2c) {
   uint32_t req_delay;
 
   /* Structure to get the pressure, temperature and humidity values */
-  struct bme280_data comp_data;
 
   /* Recommended mode of operation: Indoor navigation */
   dev.settings.osr_h = BME280_OVERSAMPLING_1X;
@@ -87,7 +89,7 @@ uint8_t BME280_Read(uint8_t address, I2C_HandleTypeDef *hi2c) {
   }
 
   dev.delay_us(req_delay, dev.intf_ptr);
-  rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
+  rslt = bme280_get_sensor_data(BME280_ALL, comp_data, &dev);
   return rslt;
 
 //      print_sensor_data(&comp_data);
